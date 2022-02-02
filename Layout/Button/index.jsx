94f619c55ee8bd68a2fd7ceb8aab54687450b56b
Krb <maurodavidcaceres@gmail.com>
2022-02-02 11:58:51 -0300

    refactor

--- Layout/Button/index.jsx
+++ Layout/Button/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Button = ({ title, width, extra }) => {
   return (
     <button
-      className={`text-black p-4 bg-primary w-[${
+      className={` bg-primary w-[${
         width ? width : "100%"
-      }] font-black select-none ${extra ? extra : ""} `}
+      }] font-black select-none ${extra ? extra : ""} text-black p-4 `}
     >
       {title ? title : "Submit"}
     </button>
