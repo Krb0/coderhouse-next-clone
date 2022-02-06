8334984b9143d9a9a5d8cca9f651eb0611b6f0f2
Krb <maurodavidcaceres@gmail.com>
2022-02-05 22:28:17 -0300

    context added

---
+++ Context/SectionContext.js
@@ -0,0 +1,14 @@
+import { createContext, useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+export const SectionContext = createContext([]);
+
+export const SectionProvider = (props) => {
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+  return (
+    <SectionContext.Provider value={[ref, inView]}>
+      {props.children}
+    </SectionContext.Provider>
+  );
+};
